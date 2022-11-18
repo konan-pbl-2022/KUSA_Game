c1dd7e82c4bd02e601573609ac08e809722b0a77
shintaku <takushin77@outlook.jp>
2022-11-18 10:53:03 +0900

    ホーム画面作った

--- app/src/main/java/com/example/kusa_game/MainActivity.java
+++ app/src/main/java/com/example/kusa_game/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.kusa_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Button = (Button) findViewById(R.id.button);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMode.class);
+                startActivity(intent);
+            }
+        });
+
+        Button Button2 = (Button) findViewById(R.id.button);
+        Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMode.class);
+                startActivity(intent);
+            }
+        });
+
+        Button Button3 = (Button) findViewById(R.id.button);
+        Button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMode.class);
+                startActivity(intent);
+            }
+        });
+
+        Button Button4 = (Button) findViewById(R.id.button);
+        Button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMode.class);
+                startActivity(intent);
+            }
+        });
+
+        Button Button5 = (Button) findViewById(R.id.button);
+        Button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMode.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
